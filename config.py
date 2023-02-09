1c94ed5382c7b378649ff60d7471ccc51032a041
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-02-09 19:25:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "11615722")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6225376312:AAHwyKTaYHVGdJdJwPHt4b_Aw7-YEpkAQEc") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "MatRixBotzTG") 
 
 DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://re:re@cluster0.julflp9.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/c1159039f103f9a9b4506.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1580642587').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5596825598').split()]
 
