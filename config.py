5325a2cf4b2073eba3310b8b3d8a190884edd5ec
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-02-16 10:07:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ API_ID = os.environ.get("API_ID", "11615722")
 
 API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6225376312:AAHwyKTaYHVGdJdJwPHt4b_Aw7-YEpkAQEc") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6091563147:AAFKAQL8MRj4E7gLKvQmYpi_Jg4KnJGNaJs") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "MatRixBotzTG") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "MLZ_BOTZ") 
 
 DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
@@ -16,9 +16,9 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://re:re@cluster0.julflp9.mongodb.n
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/c1159039f103f9a9b4506.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/8a370dfcfaf98070af522.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5596825598').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5596825598 5726662425 5845960615 1833209093').split()]
 
 PORT = os.environ.get('PORT', '8080')
 
