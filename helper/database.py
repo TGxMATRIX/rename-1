7ea58dfbfc2335c0bae2cbccbcb0ab65cbbb25ee
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-02-17 07:06:53 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,6 +1,46 @@
 import motor.motor_asyncio
 from config import DB_URL, DB_NAME
+from helper.date import add_date
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
 
+#Total User 
+
+def total_user():
+      user = dbcol.count_documents({})
+      return user
+      
+#insert bot Data 
+def botdata(chat_id):
+	bot_id = int(chat_id)
+	try:
+		bot_data = {"_id":bot_id,"total_rename":0,"total_size":0}
+		dbcol.insert_one(bot_data)
+	except:
+		pass
+
+
+def total_rename(chat_id,renamed_file):
+	now = int(renamed_file) + 1
+	dbcol.update_one({"_id":chat_id},{"$set":{"total_rename":str(now)}})
+	
+def total_size(chat_id,total_size,now_file_size):
+	now = int(total_size) + now_file_size
+	dbcol.update_one({"_id":chat_id},{"$set":{"total_size":str(now)}})
+
+	
+#insert user data 
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"file_id":None ,"caption":None ,"daily":0 ,"date":0 , "uploadlimit" :2147483648,"used_limit":0,"usertype":"Free","prexdate" : None}
+            try:
+            	dbcol.insert_one(user_det)
+            except:
+            	return True
+            	pass
 class Database:
 
     def __init__(self, uri, database_name):
